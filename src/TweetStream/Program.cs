3beb19e2e235122e10ab2e9da4c59228034bc233
Geoffrey Huntley <ghuntley@ghuntley.com>
2014-12-16 20:15:03 +1100

    initial commit

---
+++ src/TweetStream/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BoxKite.Twitter;
+using BoxKite.Twitter.Models;
+using Newtonsoft.Json;
+
+namespace TweetStream
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task t = MainAsync(args);
+            t.Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            const string filename = "credentials.json";
+
+            var json = File.ReadAllText(filename);
+            var credentials = JsonConvert.DeserializeObject<TwitterCredentials>(json);
+
+            var session = new UserSession(credentials, new DesktopPlatformAdaptor());
+            var user = await session.GetVerifyCredentials();
+
+            if (user.OK)
+            {
+                Console.WriteLine(credentials.ScreenName + " is authorised to use BoxKite.Twitter.");
+
+                var stream = session.StartSearchStream(track: "#yolo");
+                stream.FoundTweets.Subscribe(tweet =>
+                {
+                    Console.WriteLine(String.Format("ScreenName: {0}, Tweet: {1}", tweet.User.ScreenName, tweet.Text));
+                });
+                stream.Start();
+            }
+            else
+            {
+                Console.WriteLine("Credentials found in {0} were invalid, please run the Authenticator and copy over the resulting output.", filename);
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ src/TwitterAuthenticator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoxKite.Twitter;
+using BoxKite.Twitter.Authentication;
+using BoxKite.Twitter.Models;
+using Newtonsoft.Json;
+
+namespace TwitterAuthenticator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task t = MainAsync(args);
+            t.Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var twitterCredentials = new TwitterCredentials()
+            {
+                ConsumerKey = AppSettings.ApplicationConsumerKey,
+                ConsumerSecret = AppSettings.ApplicationConsumerSecret,
+            };
+
+            var twitter = new UserSession(twitterCredentials, new DesktopPlatformAdaptor());
+
+            var oathToken = await twitter.StartUserAuthentication();
+
+            Console.Write("Please enter the PIN as displayed in the new browser window: ");
+            var pin = Console.ReadLine();
+
+            var credentials = await twitter.ConfirmPin(pin, oathToken);
+
+            if (credentials.Valid)
+            {
+                const string filename = "credentials.json";
+
+                string json = JsonConvert.SerializeObject(credentials);
+
+                File.WriteAllText(filename, json);
+                Console.WriteLine("Credentials were successfully saved to '{0}'", filename);
+            }
+            else
+            {
+                Console.WriteLine("Credentials were invalid.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
